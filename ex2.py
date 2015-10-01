69c597db435962f86628b5aa9e05f495121df6c2
t-tsuchiya <t-tsuchiya@biglobe.co.jp>
2015-10-01 15:14:56 +0900

    implement ex2

--- ex2.py
+++ ex2.py
@@ -7,4 +7,15 @@ def reverse(input):
     :param input: 文字列
     :returns: 反対になった文字列
     """
+    # test result : NG
+    #output = input
+
+    # test result : OK
+    #output = 'gnirts'
+
+    output = ''
+    for i in range(0, len(input)):
+        #output = output +  str(input[i])
+        j = len(input) - i - 1
+        output += input[j]
     return output
